abf44055e461e13025fc793b3367e1bd05bd818c
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-10 17:01:04 +0530

    Conditional statements 7

---
+++ src/Problem3.java
@@ -0,0 +1,7 @@
+public class Problem3 {
+    public static void main(String[] args){
+        int year = 2024;
+        String results = year % 4 ==0 ? "its a leap year" : "Non-leap year";
+        System.out.println(results);
+    }
+}
