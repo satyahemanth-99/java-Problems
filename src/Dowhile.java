228b42d5d437c29c0da5a205ec440d85d8af2add
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-11 16:08:50 +0530

    Conditional statements 11

---
+++ src/Dowhile.java
@@ -0,0 +1,10 @@
+public class Dowhile {
+    public static void main(String[] args){
+        int a = 5;
+        do
+        {
+            System.out.println("hello " + a);
+            a++;
+        }while(a < 3);
+    }
+}
