29a9f505bd5240473ecd33e473d656b9c985247b
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-12 12:57:12 +0530

    Conditional statements 14

---
+++ src/Classnobjects01.java
@@ -0,0 +1,8 @@
+public class Classnobjects01 {
+    public static void main(String[] args){
+        int num1 = 2;
+        int num2 = 3;
+        int results = num1 + num2;
+        System.out.println(results);
+    }
+}
