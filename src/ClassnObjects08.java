8aa553f9d2f8539095c029a003379a5af8990593
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-13 09:07:07 +0530

    Conditional statements 23

---
+++ src/ClassnObjects08.java
@@ -0,0 +1,13 @@
+class mmath{
+    public int add(int n1, int n2){
+        return n1 + n2;
+    }
+    public int add(int n1, int n2, int n3){
+        return n1 + n2 +n3;
+    }
+}
+public class ClassnObjects08 {
+    static void main(String[] args) {
+        
+    }
+}
