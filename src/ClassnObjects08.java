cffcc455eb689d4fe52653d4f5e5c3b48105d950
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-13 09:10:21 +0530

    Conditional statements 24

--- src/ClassnObjects08.java
+++ src/ClassnObjects08.java
@@ -1,4 +1,4 @@
-class mmath{
+class Mmath{
     public int add(int n1, int n2){
         return n1 + n2;
     }
@@ -8,6 +8,8 @@ class mmath{
 }
 public class ClassnObjects08 {
     static void main(String[] args) {
-        
+        Mmath mat1 = new Mmath();
+        int result = mat1.add(2,3);
+        System.out.println(result);
     }
 }
