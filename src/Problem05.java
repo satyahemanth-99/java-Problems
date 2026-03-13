38503c15c7503245d5409e1562902cd0f756bf69
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-13 16:14:52 +0530

    Array01

--- src/Problem05.java
+++ src/Problem05.java
@@ -1,5 +1,13 @@
 public class Problem05 {
     static void main(String[] args) {
-        
+        int[] marks = new int[10];
+        marks[1] = 23;
+        marks[3] = 31;
+        marks[5] = 53;
+        marks[7] = 63;
+        marks[9] = 29;
+        for(int i = 0; i < marks.length; i++){
+            System.out.println(marks[i]);
+        }
     }
 }
