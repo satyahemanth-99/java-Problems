2c0706dac581b30a82e9b8e02282dfbe8435ba4a
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-13 16:18:20 +0530

    Array02

---
+++ src/Array01.java
@@ -0,0 +1,8 @@
+public class Array01 {
+    static void main(String[] args) {
+        int[] marks = new int[3];
+        for(int i = 0; i < marks.length; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
