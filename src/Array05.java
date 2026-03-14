0f5afc4c9b28583aed5c20a3f173060950cb25e6
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-14 09:17:46 +0530

    Arrays -002

--- src/Array05.java
+++ src/Array05.java
@@ -1,10 +1,6 @@
 public class Array05 {
     static void main(String[] args) {
-        int[] nums = new int[2];
-        nums[0] = 2;
-        nums[1] = 1;
-        for(int j = 0; j < nums.length; j++){
-            System.out.println(nums[j]);
-        }
+        int[] nums = {2,3,4};
+        System.out.println(nums[0]);
     }
 }
