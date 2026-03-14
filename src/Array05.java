078473268b8d223d826b8e957aac0bb45a9a7c15
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-14 09:18:58 +0530

    Arrays -003

--- src/Array05.java
+++ src/Array05.java
@@ -1,6 +1,8 @@
 public class Array05 {
     static void main(String[] args) {
         int[] nums = {2,3,4};
-        System.out.println(nums[0]);
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
     }
 }
