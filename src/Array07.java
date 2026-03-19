1e49a67e2ed131712cf58e7656870105e800956b
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-20 00:24:08 +0530

    Array_08_

---
+++ src/Array07.java
@@ -0,0 +1,14 @@
+public class Array07 {
+    static void main(String[] args) {
+        int[] a = {1,3,5,2,6};
+        int[] b = {2,3,1,8,3};
+        for(int i = 0; i < a.length; i++){
+
+            for(int j = 0; j < b.length; j++){
+                if(a[i] == b[j]){
+                    int[] c = {a[i]};
+                };
+            }
+        }
+    }
+}
---
+++ src/Array08.java
@@ -0,0 +1,8 @@
+public class Array08 {
+    static void main(String[] args) {
+        int num[] = new int[5];
+        for(int i = 0; i<num.length; i++){
+            System.out.println(num[i]);
+        }
+    }
+}
