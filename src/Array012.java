0368d6ef683f4002127c54ce70e5ef1160fcf0e7
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-21 12:30:39 +0530

    Array 012

---
+++ src/Array012.java
@@ -0,0 +1,16 @@
+public class Array012 {
+    static void main(String[] args) {
+        int[][] nums = new int[3][6];
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 6; j++){
+                nums[i][j] = (int)(Math.random() * 20);
+            }
+        }
+        for(int a = 0; a < 3; a++){
+            for(int b = 0; b < 6; b++){
+                System.out.print(nums[a][b] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
