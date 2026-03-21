4981e5b4dfd90e69e95a83d516f8a836176a8e75
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-21 23:58:42 +0530

    Aloop01

---
+++ src/Aloop01.java
@@ -0,0 +1,8 @@
+public class Aloop01 {
+    static void main(String[] args) {
+        String[] fruits = {"Mango","Apple","Banana"};
+        for(String frut : fruits){
+            System.out.println(frut);
+        }
+    }
+}
