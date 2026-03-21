4060dc92cb16dd0bc55a54de12249945646b447f
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-21 11:24:26 +0530

    Array 010

---
+++ src/Array010.java
@@ -0,0 +1,11 @@
+public class Array010 {
+    static void main(String[] args) {
+        int[][] num = new int[3][4];
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
