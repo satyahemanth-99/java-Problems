41661018b810dd0891f7df851f6e4bc7ca2291d9
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-22 00:06:23 +0530

    Aloop03

---
+++ src/Aloop03.java
@@ -0,0 +1,11 @@
+public class Aloop03 {
+    static void main(String[] args) {
+        int[][] nums = new int[2][3];
+        for(int n[] : nums){
+            for(int i : n){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
