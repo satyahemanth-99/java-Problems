3125139174d4fe9de89350a99dbdae49e84d991a
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-22 00:00:44 +0530

    Aloop02

---
+++ src/Aloop02.java
@@ -0,0 +1,8 @@
+public class Aloop02 {
+    static void main(String[] args) {
+        int[] nums = {23, 45, 11, 35, 65, 21, 12};
+        for(int num : nums){
+            System.out.println(num);
+        }
+    }
+}
