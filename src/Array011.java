9da0433a0a85248d3471009699c4fa43e0d63acc
satyahemanth-99 <motipallisatyahemanth@gmail.com>
2026-03-21 12:13:12 +0530

    Array 011-1

--- src/Array011.java
+++ src/Array011.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Array011 {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = (int)(Math.random() * 10);
-        System.out.println("random number was " + num);
+        int num = -1;
 
         int hum = 0;
         while( hum != num){
+            num = (int)(Math.random() * 10);
+            System.out.println("random number was " + num);
             System.out.println("Enter your number here: ");
             hum = sc.nextInt();
             if(hum > num){
@@ -16,8 +17,15 @@ public class Array011 {
                 System.out.println("you guess  too low");
             }else {
                 System.out.println("congrats you guess correctly");
-                
+                System.out.println("Do you want to play again? 'Yes'/ 'No' ");
+                String ans = sc.next();
+                if(ans.equals("Yes")){
+                    hum = 0;
+                }else {
+                    System.out.println("thankyou for your visiting");
+                }
             }
         }
+        sc.close();
     }
 }
